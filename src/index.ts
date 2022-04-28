03b04d95bfc0f7e2d4d8be8ddd6d9812d3ff18eb
dougflip <douglasdifilippo@gmail.com>
2022-04-28 10:34:22 -0400

    Try a community action to tag and push to GitHub

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export type HelloWorldArgs = {
   name: string;
 };
 
+/**
+ * Returns a greeting string based on the provided name.
+ */
 export function helloWorld({ name }: HelloWorldArgs): string {
   return `Hello ${name}`;
 }
